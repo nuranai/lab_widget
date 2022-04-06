c192c1833fefd46184e0c31c385de8dc7cf02c91
nuranai <bajanga07@gmail.com>
2022-04-06 22:28:12 +0700

    finished example 2.8
    todo: write comments

--- main.cpp
+++ main.cpp
@@ -1,26 +1,11 @@
 #include "mainwindow.h"
-#include <QtGui>
 #include <QApplication>
-#include <QLabel>
-#include <QPushButton>
-#include <QHBoxLayout>
+#include "win.h"
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-
-    w.setWindowTitle("Hello"); // изменение заголовка окна
-    QLabel *helloLabel=new QLabel("Hello!",&w); // создание метки
-    QPushButton *exitButton=new QPushButton("Close",&w);
-    // создание кнопки
-    QHBoxLayout *layout = new QHBoxLayout(&w); // создание
-    // менеджера компоновки для управления размещением метки и кнопки в окне win
-    layout->addWidget(helloLabel); // добавление метки к компоновщику
-    layout->addWidget(exitButton); // добавление кнопки к компоновщику
-    // связь сигнала нажатия кнопки с закрытием окна win
-    QObject::connect(exitButton,SIGNAL(clicked(bool)),
-                     &w,SLOT(close()));
-    w.show();
-    return a.exec();
+    QApplication app(argc, argv);
+    Win win(0);
+    win.show();
+    return app.exec();
 }
---
+++ win.h
@@ -0,0 +1,42 @@
+#ifndef WIN_H
+#define WIN_H
+
+#include <QWidget>
+#include <QtGui>
+#include <QLineEdit>
+#include <QLabel>
+#include <QPushButton>
+
+class Counter:public QLineEdit
+{
+    Q_OBJECT
+public:
+    Counter(const QString & contents, QWidget *parent=0):
+        QLineEdit(contents,parent){}
+signals:
+    void tick_signal();
+public slots:
+    void add_one()
+    {
+        QString str=text();
+        int r=str.toInt();
+        if (r!=0 && r%5 ==0) emit tick_signal();
+        r++;
+        str.setNum(r);
+        setText(str);
+    }
+};
+
+class Win: public QWidget
+{
+    Q_OBJECT
+protected:
+    QLabel *label1,*label2;
+    Counter *edit1,*edit2;
+    QPushButton *calcbutton;
+    QPushButton *exitbutton;
+public:
+    Win(QWidget *parent = 0);
+};
+
+#endif // WIN_H
